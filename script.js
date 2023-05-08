4e2e6d9e265e6f140f7613d9bfa08f2597c0836e
Alibek <alibek00@mail.com>
2023-05-08 22:25:49 +0500

    added responsive design

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+};
+
+
+let img1 = document.querySelector('#img1');
+let cards1 = document.querySelector('.cards1');
+
+img1.onclick = () => {
+    img1.classList.toggle('bx');
+    cards1.classList.toggle('opencard');
 }
